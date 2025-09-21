e86a0323eaca05c5ce0e6e210c5de827e8a123a6
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-20 19:01:15 -0500

    feat(extensions): add string extensions for camel case naming

---
+++ TestPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Humanizer;
+
+namespace TestPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+/// <summary>
+///     String extensions
+/// </summary>
+/// <remarks>
+///     This class contains extension methods for strings.
+///     It includes methods to convert strings to camel case and pluralize them.
+/// </remarks>
+public static class StringExtensions
+{
+    /// <summary>
+    ///     Convert a string to camel case
+    /// </summary>
+    /// <param name="text">The string to convert</param>
+    /// <returns>The string converted to camel casea</returns>
+    public static string ToCamelCase(this string text)
+    {
+        if (string.IsNullOrEmpty(text) || char.IsLower(text[0]))
+            return text;
+
+        return char.ToLowerInvariant(text[0]) + text[1..];
+    }
+
+    /// <summary>
+    ///     Pluralize a string
+    /// </summary>
+    /// <param name="text">The string to convert</param>
+    /// <returns>The string converted to plural</returns>
+    public static string ToPlural(this string text)
+    {
+        return text.Pluralize(false);
+    }
+}
