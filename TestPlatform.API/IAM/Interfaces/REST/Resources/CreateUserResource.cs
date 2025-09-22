bc86a89e06fb4c544658c230cc139263f31af509
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-21 19:07:11 -0500

    feat(users): add resources

---
+++ TestPlatform.API/IAM/Interfaces/REST/Resources/CreateUserResource.cs
@@ -0,0 +1,11 @@
+﻿using TestPlatform.API.Shared.Domain.Model.ValueObjects;
+
+namespace TestPlatform.API.IAM.Interfaces.REST.Resources;
+
+/// <summary>
+///     Resource for creating a new user
+/// </summary>
+public record CreateUserResource(
+        Email UserName,
+        string Password
+    );
---
+++ TestPlatform.API/IAM/Interfaces/REST/Resources/UserResource.cs
@@ -0,0 +1,9 @@
+﻿namespace TestPlatform.API.IAM.Interfaces.REST.Resources;
+
+/// <summary>
+///     Resource class to define which data of a user should be exposed via the REST API.
+/// </summary>
+public record UserResource(
+        string Id,
+        string Username
+    );
