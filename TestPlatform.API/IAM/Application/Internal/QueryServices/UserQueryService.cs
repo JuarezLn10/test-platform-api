74c5a9037e51af311405642bca6092049831717d
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-21 19:01:38 -0500

    feat(users): add query service implementation

---
+++ TestPlatform.API/IAM/Application/Internal/QueryServices/UserQueryService.cs
@@ -0,0 +1,28 @@
+﻿using TestPlatform.API.IAM.Domain.Model.Aggregates;
+using TestPlatform.API.IAM.Domain.Model.Queries;
+using TestPlatform.API.IAM.Domain.Repositories;
+using TestPlatform.API.IAM.Domain.Services;
+
+namespace TestPlatform.API.IAM.Application.Internal.QueryServices;
+
+/// <summary>
+///     Implements user-related query operations.
+/// </summary>
+public class UserQueryService(
+        IUserRepository userRepository
+    ) : IUserQueryService
+{
+    /// <summary>
+    ///     Query handler for GetAllUsersQuery
+    /// </summary>
+    /// <param name="query">
+    ///     The query object containing parameters for fetching all users.
+    /// </param>
+    /// <returns>
+    ///     The list of users matching the query criteria.
+    /// </returns>
+    public async Task<IEnumerable<User>> Handle(GetAllUsersQuery query)
+    {
+        return await userRepository.GetAllAsync();
+    }
+}
