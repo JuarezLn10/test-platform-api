75d301bb15208c9e19e104b50766345e1cd0070b
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-21 19:12:25 -0500

    feat(users): add assemblers

---
+++ TestPlatform.API/IAM/Interfaces/REST/Assemblers/CreateUserCommandFromResourceAssembler.cs
@@ -0,0 +1,27 @@
+﻿using TestPlatform.API.IAM.Domain.Model.Commands;
+using TestPlatform.API.IAM.Interfaces.REST.Resources;
+
+namespace TestPlatform.API.IAM.Interfaces.REST.Assemblers;
+
+/// <summary>
+///     Static assembler class to convert CreateUserResource to CreateUserCommand
+/// </summary>
+public static class CreateUserCommandFromResourceAssembler
+{
+    /// <summary>
+    ///     Transforms CreateUserResource to CreateUserCommand
+    /// </summary>
+    /// <param name="resource">
+    ///     The CreateUserResource to be transformed
+    /// </param>
+    /// <returns>
+    ///     The corresponding CreateUserCommand
+    /// </returns>
+    public static CreateUserCommand ToCommandFromResource(CreateUserResource resource)
+    {
+        return new CreateUserCommand(
+                resource.UserName,
+                resource.Password
+            );
+    }
+}
---
+++ TestPlatform.API/IAM/Interfaces/REST/Assemblers/UserResourceFromEntityAssembler.cs
@@ -0,0 +1,27 @@
+﻿using TestPlatform.API.IAM.Domain.Model.Aggregates;
+using TestPlatform.API.IAM.Interfaces.REST.Resources;
+
+namespace TestPlatform.API.IAM.Interfaces.REST.Assemblers;
+
+/// <summary>
+///     Static assembler class to convert User entity to UserResource.
+/// </summary>
+public static class UserResourceFromEntityAssembler
+{
+    /// <summary>
+    ///     Transforms a User entity into a UserResource.
+    /// </summary>
+    /// <param name="entity">
+    ///     The User entity to be transformed.
+    /// </param>
+    /// <returns>
+    ///     The corresponding UserResource.
+    /// </returns>
+    public static UserResource ToResourceFromEntity(User entity)
+    {
+        return new UserResource(
+                entity.Id.ToString(),
+                entity.UserName.GetValue
+            );
+    }
+}
